cf54684144110c22d04f54a7c0a4c7ea39f21063
Varun Goyal <varungoyalkkp1@gmail.com>
2026-04-09 21:20:49 +0530

    config suspens

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,15 @@
 import LoginUI from "@/modules/auth/components/login-ui";
 import { requireUnAuth } from "@/modules/auth/utils/auth-utils";
+import { Suspense } from "react";
 
 const LoginPage = async () => {
 	await requireUnAuth()
 
 	return (
 		<div>
-			<LoginUI />
+		 <Suspense fallback={<div>Loading...</div>}>
+      <LoginUI />
+    </Suspense>
 		</div>
 	);
 };
